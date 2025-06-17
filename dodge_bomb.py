c653943d0a634bcd458a57457e9663e51a3b4d15
Ryosei <c0a240966c@edu.teu.ac.jp>
2025-06-17 17:11:09 +0900

    演習問題2拡大、加速

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -33,7 +33,7 @@ def gameover(screen: pg.Surface) -> None:
     こうかとんが爆弾に当たった時のゲームオーバー画面を表示する関数
 
     引数:
-        screen: Pygame画面Surface
+        screen: 黒画面
     """
     # 半透明の黒い背景を表示
     black = pg.Surface((WIDTH, HEIGHT))
@@ -53,22 +53,40 @@ def gameover(screen: pg.Surface) -> None:
 
     pg.display.update()
     time.sleep(5) #時を5秒止める
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    爆弾の大きさに応じたSurfaceのリストと加速度リストを返す関数
+
+    戻り値:
+        爆弾画像リスト, 加速度リスト
+    """
+    bb_imgs = []  # 爆弾画像のリスト
+    bb_accs = [a for a in range(1, 11)]  # 加速度リスト
+    for r in range(1, 11):  # 1倍〜10倍
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))  # 黒背景を透明に
+        bb_imgs.append(bb_img)
+        bb_accs.append(r)  # 加速度：1〜10
+    return bb_imgs, bb_accs
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_img = get_kk_img((0, 0))
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     bb_img = pg.Surface((20,20)) # 透明背景の画像
     pg.draw.circle(bb_img, (255,0,0),(10,10),10) # 中心に赤い円（爆弾）
     bb_img.set_colorkey((0, 0, 0))
     bb_rct = bb_img.get_rect()
+    
     bb_rct.center = (random.randint(0, WIDTH), random.randint(0, HEIGHT))  # ランダムな位置に配置
     clock = pg.time.Clock()
     tmr = 0
     vx, vy = +5, +5
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -84,15 +102,25 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        # screen.blit(kk_img, kk_rct)
+        kk_img = get_kk_img(tuple(sum_mv))  # 向きに応じて画像を回転
         screen.blit(kk_img, kk_rct)
+        
+        #拡大、加速
+        bb_imgs, bb_accs = init_bb_imgs()
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        avy = vy*bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_rct.move_ip(avx, avy)
         bb_rct.move_ip(vx, vy)
+
         yoko, tate = check_bound(bb_rct) 
         if not yoko: #横方向にはみ出ていたら
             vx *= -1
         if not tate: #縦方向にはみ出ていたら
             vy *= -1
         
-        if kk_rct.colliderect(bb_rct):
+        if kk_rct.colliderect(bb_rct): #ゲームオーバー
             gameover(screen)
             return
         
